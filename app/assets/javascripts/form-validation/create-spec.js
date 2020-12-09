adfef985f834d76a9dc6dd81132a9d0345ad30ae
Alex Painter <alex@kingsley.media>
2020-12-09 11:24:38 +0000

    Update create-spec.js

--- app/assets/javascripts/form-validation/create-spec.js
+++ app/assets/javascripts/form-validation/create-spec.js
@@ -122,12 +122,16 @@ function getProviderData() {
 
 document.getElementById('fundingStreamSpecification').onchange = function getfundingStreamSpecficiation() {
     let fundingStreamSpecficiationText = fundingStreamSpecification.options[fundingStreamSpecification.selectedIndex].value;
+    let fundingStreamSpecficiation = fundingStreamSpecification.options[fundingStreamSpecification.selectedIndex].text;
     console.log(fundingStreamSpecficiationText)
+    console.log(fundingStreamSpecficiation)
     sessionStorage.setItem("funding stream", fundingStreamSpecficiationText)
 
-    let n = fundingStreamSpecficiationText.includes("FDZ");
+    let n = fundingStreamSpecficiation.includes("16 to 19")
+    let m = fundingStreamSpecficiation.includes("General Annual Grant")
     console.log(n)
-    if (n == true) {
+    console.log(m)
+    if (n == true || m == true ) {
         document.getElementById("providerData-error-group").classList.remove("govuk-visually-hidden")
         document.getElementById("coreProviderData-group").classList.add("govuk-visually-hidden")
     }
