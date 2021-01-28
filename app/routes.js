e8bd0b044a2e47e34650f41a5f8e4b2c8d00b3dc
bethanphillimore <bethan.phillimore@gmail.com>
2021-01-28 15:25:45 +0000

    validation

--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,5 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
-
+router.use('/node_modules', express.static('node_modules'))
 module.exports = router
